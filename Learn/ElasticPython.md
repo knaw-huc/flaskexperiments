41f5329c6f6e9ec339eda66de7b4d69e5ad5ce72
Maarten van der Peet <Maarten.van.der.Peet@di.huc.knaw.nl>
2021-07-01 14:48:39 +0200

    different ways

--- Learn/ElasticPython.md
+++ Learn/ElasticPython.md
@@ -1,8 +1,17 @@
 # ElasticSearch & Python
 
-## Python as a prettiefyer for curl
+## Python for a prettier output for json output obtained from curl
 
     curl http://localhost:9200/favouritesongs/nummer/1 | python -m json.tool
 
+or 
 
 
+    curl http://localhost:9200/favouritesongs/nummer/1&pretty=true 
+
+But this is only for ElasticSearch, not other api's.
+
+
+
+json.dumps(dict) # converts python structures, dictionary or list to json strings
+dicdata = json.loads(data) # json-string to dictionary
--- elastic.py
+++ elastic.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+from os import closerange
 import requests
 import json
 
 # response  = requests.get("http://localhost:5000/endpoint/")
 # print(response.text)
 
-endpoint = "http://localhost:9200/favouritesongs/nummer/" # elastic search
+endpoint = "http://localhost:9200/favouritesongs/nummer/" # ElasticSearch endpoint
 
+# a json string must first converted to a dictionary
 data = '''
 {
     "id": 1,
@@ -20,8 +22,20 @@ data = '''
 }    
 '''
 
-# https://www.youtube.com/watch?v=2cXDgFwE13g
+dicdata = json.loads(data) # json-string to dictionary
+id = dicdata["id"]
+unique_endpoint = endpoint + str(id)
+print(unique_endpoint)
+headers = {'content-type': 'application/json'}
 
+r = requests.put(unique_endpoint,data = json.dumps(dicdata), headers=headers)
+
+''' test if it succesfull updated posted data '''
+
+response = requests.get(unique_endpoint);
+# response.json() obtain a dictionary
+json_formatted_str = json.dumps(response.json(), indent=4)
+print(json_formatted_str)
 
 datapythonformat = {
     "id" : 2,
@@ -34,23 +48,23 @@ datapythonformat = {
     "url": "https://www.youtube.com/watch?v=R-O5U4uuPF8"
 }
 
-
-
-
-dicdata = json.loads(data) # json-string to dictionary
-print(dicdata)
-id = dicdata["id"]
-endpoint += str(id)
-
-# json.dumps(dict) converts python structures, dictionary or list to json
+id = datapythonformat["id"]
+unique_endpoint = endpoint + str(id)
+print(unique_endpoint)
 
 headers = {'content-type': 'application/json'}
 
+r = requests.put(unique_endpoint,data = json.dumps(datapythonformat), headers=headers)
+response = requests.get(unique_endpoint);
+# response.json() obtain a dictionary
+json_formatted_str = json.dumps(response.json(), indent=4)
+print(json_formatted_str)
 
-r = requests.put(endpoint,data = json.dumps(dicdata), headers=headers)
 
-response = requests.get(endpoint);
+def dump(json):
+    return json.dumps(json, indent=4)
 
-# print(type(response), response, response.json())
 
-print(response.json())
+# print(type(response.json()))
+# d = response.json()
+# print(type(d), d, d['_source']['artiest'])
